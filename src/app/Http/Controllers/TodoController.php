24a6eb929164e61441c5847f56ca1e748e2c462f
Your Name <you@example.com>
2025-01-22 19:04:30 +0900

    feat:新規作成処理の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -16,8 +16,21 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
+    
     public function create()
     {
         return view('todo.create');
+    }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+    
+        $todo = new Todo();
+        $todo->content = $content;
+        $todo->save();
+
+        return redirect()->route('todo.index');
 }
+
 }
