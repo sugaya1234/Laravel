fb58f59bd1ee4631f1f64e0243996e9d6f760c70
Your Name <you@example.com>
2025-01-29 17:24:28 +0900

    feat:ToDo詳細表示機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -31,6 +31,14 @@ class TodoController extends Controller
         $todo->save();
 
         return redirect()->route('todo.index');
-}
+    }
+
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+
+        return view('todo.show', ['todo' => $todo]);
+    }
 
 }
