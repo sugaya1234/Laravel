e31ed616148c8561ed54973fee0df0c92a7543f7
Your Name <you@example.com>
2025-01-31 09:20:34 +0900

    feat:編集画面の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -43,4 +43,11 @@ class TodoController extends Controller
         return view('todo.show', ['todo' => $todo]);
     }
 
+    public function edit($id)
+{
+    $todo = $this->todo->find($id);
+
+    return view('todo.edit', ['todo' => $todo]);
+}
+
 }
