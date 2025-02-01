9ddaddb044bb553ed6e9ede3b415a4a98b6ade22
Your Name <you@example.com>
2025-02-01 09:23:14 +0900

    feat: バリデーションの実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Http\Requests\TodoRequest;
 use App\Todo;
 
 class TodoController extends Controller
@@ -27,7 +26,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
     
@@ -50,12 +49,12 @@ class TodoController extends Controller
     return view('todo.edit', ['todo' => $todo]);
 }
 
-public function update(Request $request, $id)
+public function update(TodoRequest $request, $id)
 {
     $inputs = $request->all();
-    
     $todo = $this->todo->find($id);
-    $todo->fill($inputs)->save();
+    $todo->fill($inputs);
+    $todo->save();
 
     return redirect()->route('todo.show', $todo->id);
 }
--- src/resources/views/todo/create.blade.php
+++ src/resources/views/todo/create.blade.php
@@ -6,12 +6,14 @@
         <div class="card-header">ToDo作成</div>
         <div class="card-body">
           <form method="POST" action="{{ route('todo.store') }}">
-          @csrf
-          @method('PUT')
+                      @csrf
             <div class="form-group row">
               <label for="name" class="col-md-4 col-form-label text-md-right">ToDo入力</label>
               <div class="col-md-6">
-                <input type="text" class="form-control" name="content" value="">
+              <input type="text" class="form-control @if($errors->has('content')) border-danger @endif" name="content" value="">
+    @if($errors->has('content'))
+      <span class="text-danger">{{ $errors->first('content') }}</span>
+    @endif
               </div>
             </div>
             <div class="form-group row mb-0">
